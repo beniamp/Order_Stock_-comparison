3f4bccaf61f53903faf50685e6692040c7d7fcba
beniamp <102410523+beniamp@users.noreply.github.com>
2024-09-08 13:05:03 +0330

    Update ord_stock_comparison.py

--- ord_stock_comparison.py
+++ ord_stock_comparison.py
@@ -86,7 +86,7 @@ filtered_stc = filtered_stc[(filtered_stc['Gregorian_Date'] >= start_date) &
 
 
 agg_stock = filtered_stc.groupby(['Name', 'Color', 'Date_Formatted', 'Category', 'Brand']).agg({'Quantity': 'sum', 'BasePrice': 'min'}).reset_index()
-agg_order = filtered_ord.groupby.groupby(['ProductColorName', 'ColorName', 'Date_Formatted', 'Category']).agg({'Quantity': 'sum', 'BasePrice': 'max'}).reset_index()
+agg_order = filtered_ord.groupby(['ProductColorName', 'ColorName', 'Date_Formatted', 'Category']).agg({'Quantity': 'sum', 'BasePrice': 'max'}).reset_index()
 
 
 
